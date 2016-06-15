abbb38c3add104cce8c0a81b449de11983d29f64
root <christyler@126.com>
2016-06-14 17:54:13 -0700

    new file:   .idea/vcs.xml 	new file:   DnsSwift/DnsSwift.iml 	new file:   DnsSwift/src/DnsSwift2Kifer/Tester1.java 	new file:   output/production/DnsSwift/DnsSwift2Kifer/Tester1.class

---
+++ DnsSwift/src/DnsSwift2Kifer/Tester1.java
@@ -0,0 +1,10 @@
+package DnsSwift2Kifer;
+
+/**
+ * Created by root on 6/14/16.
+ */
+public class Tester1 {
+    public static void main(String [] args){
+        System.out.println("THis is the first line of the java file.");
+    }
+}
